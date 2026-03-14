5940fec35b45922335774ccdfb6c2063e6ec6d63
Talha Ali <42632372+zonat-tk@users.noreply.github.com>
2026-03-13 18:42:42 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+##########################################
+# BITWARDEN INSTALL SCRIPT BY TALHA ALI
+# Fancy Look for the Terminal
+# www.zonatsolutions.com
+##########################################
